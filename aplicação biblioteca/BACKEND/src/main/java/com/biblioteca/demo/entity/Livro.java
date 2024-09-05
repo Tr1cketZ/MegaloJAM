84a4a0afcddcd5b252315a81e14566408b65d29e
VICTOR BEHR PEREIRA MENDES <victor.mendes@alunos.sc.senac.br>
2024-09-05 19:38:47 -0300

    fix: Getters e Setters do Backend
    Foi notado a ausencia dos getter e setters do backend e foi corrigido.

--- aplicação biblioteca/BACKEND/src/main/java/com/biblioteca/demo/entity/Livro.java
+++ aplicação biblioteca/BACKEND/src/main/java/com/biblioteca/demo/entity/Livro.java
@@ -15,5 +15,45 @@ public class Livro {
     private String isbn;
     private int numeroExemplares;
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNumeroExemplares() {
+        return this.numeroExemplares;
+    }
+
+    public void setNumeroExemplares(int numeroExemplares) {
+        this.numeroExemplares = numeroExemplares;
+    }
+
     // Getters and setters omitted for brevity
 }
--- aplicação biblioteca/BACKEND/src/main/java/com/biblioteca/demo/entity/Usuario.java
+++ aplicação biblioteca/BACKEND/src/main/java/com/biblioteca/demo/entity/Usuario.java
@@ -15,5 +15,45 @@ public class Usuario {
     private String senha;
     private String role;
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     // Getters and setters omitted for brevity
 }
