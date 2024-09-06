9badbd87189353386118f61ef7fa82d2a92d03ea
Tr1cketZ <arrasa888@gmail.com>
2024-09-06 16:19:37 -0300

    fix: Backend concertado
    Backend estava faltando componentes e os scripts estavam desatualizados para as novas versões do Springboot.

--- aplicação biblioteca/BACKEND/demo/src/main/java/com/biblioteca/demo/security/WebSecurityConfig.java
+++ aplicação biblioteca/BACKEND/demo/src/main/java/com/biblioteca/demo/security/WebSecurityConfig.java
@@ -4,24 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/usuarios/**").hasRole("ADMIN")
+            .requestMatchers("/api/usuarios/**").hasRole("ADMIN")  
             .anyRequest().authenticated()
             .and()
             .formLogin().permitAll()
             .and()
             .logout().permitAll();
+
+        return http.build();
     }
 
     @Bean
